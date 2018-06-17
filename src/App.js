e47cccefd0bb0e13771c77362c1034df59a7d80c
Lim Kimhuor <kimhuorlim@gmail.com>
2018-06-18 01:30:52 +0700

    View login and user info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UserInfo from "./components/UserInfo";
 
 class App extends Component {
   render() {
@@ -8,11 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to React JS Social Oauth Login App
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UserInfo />
       </div>
     );
   }
---
+++ src/components/UserInfo.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Login from "./Login";
+
+class UserInfo extends Component {
+  render() {
+    return <Login />;
+    return (
+      <div className="user-info">
+        <label>Email:</label>
+        <span type="text" id="email">
+          test@test.com
+        </span>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
