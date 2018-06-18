6661849f542ac187b34150100be74ce9f23a1632
limkimhuor <kimhuorlim@gmail.com>
2018-06-18 13:34:21 +0700

    Integrate Git and FB login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,12 @@ import React from "react";
 const Login = props => (
   <div className="login">
     <p>Please click button below to sign in!</p>
-    <button className="github">Log In With Gitub</button>
-    <button className="facebook">Log In With Facebook</button>
+    <button className="github" onClick={() => props.authenticate("Github")}>
+      Log In With Github
+    </button>
+    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
+      Log In With Facebook
+    </button>
   </div>
 );
 
